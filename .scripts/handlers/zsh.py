aadd940fa89e629c6b15a93a935c757e0fb25d10
Bryant.Tseng <work.bryant.tseng@gmail.com>
2024-05-19 19:12:37 +0900

    fix

--- .scripts/handlers/zsh.py
+++ .scripts/handlers/zsh.py
@@ -39,12 +39,12 @@ class ZshHandler(Handler):
         # zsh-syntax-highlighting
         logger.info("[zsh][oh-my-zsh][plugin] installing zsh-syntax-highlighting")
         self.run_shell_cmd("""
-                           git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+                           git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
                            """)
         # zsh-autosuggestions
         logger.info("[zsh][oh-my-zsh][plugin] installing zsh-syntax-autosuggestions")
         self.run_shell_cmd("""
-                           git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+                           git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
                            """)
         
 
